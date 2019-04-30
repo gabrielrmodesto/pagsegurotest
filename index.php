d7a8601591f98777fc462148d6eef4ef5410b38d
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-04-30 14:21:02 -0300

    APENAS UM TESTE

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
+    <p>Teste</p>
     <button onclick="pagamento()">Pagar</button>
     <span class="endereco" data-endereco="<?=URL;?>"></span>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
