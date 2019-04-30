135287e439f81a4ad66e8e65e871633f18b1e03d
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-04-30 14:31:55 -0300

    Função para listar meios de pagamentos

--- js/pagar.js
+++ js/pagar.js
@@ -7,5 +7,25 @@ function pagamento(){
         success: function (retorno){
             PagSeguroDirectPayment.setSessionId(retorno.id);
         }
+        complete: function(retorno) {
+            // Callback para todas chamadas.
+            listarMeiosPagamentos();
+        }
+    });
+}
+
+function listarMeiosPagamentos(){
+    PagSeguroDirectPayment.getPaymentMethods({
+        amount: 500.00,
+        success: function(retorno) {
+            // Retorna os meios de pagamento disponíveis.
+            console.log(retorno);
+        },
+        error: function(retorno) {
+            // Callback para chamadas que falharam.
+        },
+        complete: function(retorno) {
+            // Callback para todas chamadas.
+        }
     });
 }
