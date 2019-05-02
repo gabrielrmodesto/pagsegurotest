e1dd5eafee87d6fdd892c60e23c5292c64df73ee
gabrielmodesto <gabriel.modesto@nexttecnologiadainformacao.com.br>
2019-05-02 16:24:32 -0300

    Adicionando os campos

--- config.php
+++ config.php
@@ -9,9 +9,13 @@ if($sandbox){
     define("TOKEN_PAGSEGURO", "88CA315883C44686BAD5C8AF3FD830A2");
     define("URL_PAGSEGURO", "https://ws.sandbox.pagseguro.uol.com.br/v2");
     define("SCRIPT_PAGSEGURO", "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js");
+    define("EMAIL_LOJA", "E-mail de suporte pós venda");
+    define("MOEDA_PAGAMENTO", "BRL");
 }else{
     define("EMAIL_PAGSEGURO", "gabriel.modesto@nexttecnologiadainformacao.com.br");
     define("TOKEN_PAGSEGURO", "02235d7b-f337-4389-806d-c60f448c9c6c1ff0edc642e3ac5bcf424d710288f2d15baf-2421-43b1-a303-0ca71a6a68e4");
     define("URL_PAGSEGURO", "https://ws.pagseguro.uol.com.br/v2");
     define("SCRIPT_PAGSEGURO", "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js");
+    define("EMAIL_LOJA", "E-mail de suporte pós venda");
+    define("MOEDA_PAGAMENTO", "BRL");
 }
--- index.php
+++ index.php
@@ -12,35 +12,155 @@
     <span class="endereco" data-endereco="<?=URL;?>"></span>
     <span id="msg-erro"></span>
     <form action="" id="formPagamento">
-        <label>Número do cartão</label>
-        <input type="text" name="numCartao" id="numCartao"><br>
+        <h2>Dados da Compra</h2>
 
-        <label>Bandeira do cartão</label>
-        <input type="text" name="bandeiraCartao" id="bandeiraCartao"><br>
+        <label>Método de Pagamento</label>
+        <input type="text" name="paymentMethod" id="paymentMethod" value="creditCard"><br><br>
+
+        <label>E-mail da Loja</label>
+        <input type="text" name="receiverEmail" id="receiverEmail" value="<?php echo EMAIL_LOJA; ?>"><br><br>
+
+        <label>Moeda de Pagamento</label>
+        <input type="text" name="currency" id="currency" value="<?php echo MOEDA_PAGAMENTO; ?>"><br><br>
+
+        <label>Desconto ou Taxa</label>
+        <input type="text" name="extraAmount" id="extraAmount" value=""><br><br>
+
+        <label>ID do Item</label>
+        <input type="text" name="itemId1" id="itemId1" value="0001"><br><br>
+
+        <label>Descrição do Item</label>
+        <input type="text" name="itemDescription1" id="itemDescription1" value="Curso de PHP Orientado a Objetos"><br><br>
+
+        <label>Preço do Item</label>
+        <input type="text" name="itemAmount1" id="itemAmount1" value="600.00"><br><br>
+
+        <label>Quantidade do Item</label>
+        <input type="text" name="itemQuantity1" id="itemQuantity1" value="1"><br><br>
+
+        <label>URL de Notificação</label>
+        <input type="text" name="notificationURL" id="notificationURL" value="<?php echo URL_NOTIFICACAO; ?>"><br><br>
+
+        <label>Reference do Item</label>
+        <input type="text" name="reference" id="reference" value="1001"><br><br>
+
+        <h2>Dados do Comprador</h2>
+
+        <label>Nome</label>
+        <input type="text" name="senderName" id="senderName" placeholder="Nome completo" required><br><br>
+
+        <label>Data de Nascimento</label>
+        <input type="text" name="creditCardHolderBirthDate" id="creditCardHolderBirthDate" placeholder="Data de Nascimento. Ex: 12/12/1912" required><br><br>
+
+        <label>CPF</label>
+        <input type="text" name="senderCPF" id="senderCPF" placeholder="CPF sem traço" required><br><br>
+
+        <label>Telefone</label>
+        <input type="text" name="senderAreaCode" id="senderAreaCode" placeholder="DDD" required>
+        <input type="text" name="senderPhone" id="senderPhone" placeholder="Somente número" required><br><br>
+
+        <label>E-mail</label>
+        <input type="email" name="senderEmail" id="senderEmail" placeholder="E-mail do comprador" required><br><br>
+
+        <h2>Endereço de Entrega</h2>
+        <label>Entrega</label>
+        <input type="text" name="shippingAddressRequired" id="shippingAddressRequired" value="true" required><br><br>
+
+        <label>Logradouro</label>
+        <input type="text" name="shippingAddressStreet" id="shippingAddressStreet" placeholder="Av. Rua"><br><br>
+
+        <label>Número</label>
+        <input type="text" name="shippingAddressNumber" id="shippingAddressNumber" placeholder="Número"><br><br>
+
+        <label>Complemento</label>
+        <input type="text" name="shippingAddressComplement" id="shippingAddressComplement" placeholder="Complemento"><br><br>
+
+        <label>Bairro</label>
+        <input type="text" name="shippingAddressDistrict" id="shippingAddressDistrict" placeholder="Bairro"><br><br>
+
+        <label>CEP</label>
+        <input type="text" name="shippingAddressPostalCode" id="shippingAddressPostalCode" placeholder="CEP sem traço"><br><br>
+
+        <label>Cidade</label>
+        <input type="text" name="shippingAddressCity" id="shippingAddressCity" placeholder="Cidade"><br><br>
+
+        <label>Estado</label>
+        <input type="text" name="shippingAddressState" id="shippingAddressState" placeholder="Sigla do Estado"><br><br>
+
+        <label>País</label>
+        <input type="text" name="shippingAddressCountry" id="shippingAddressCountry" value="BRL"><br><br>
+
+        <label>Frete</label>
+        <input type="radio" name="shippingType" value="1"> PAC
+        <input type="radio" name="shippingType" value="2"> SEDEX
+        <input type="radio" name="shippingType" value="3"> Sem frete<br><br>
+
+        <label>Valor Frete</label>
+        <input type="text" name="shippingCost" id="senderCPF" placeholder="Preço do frete. Ex: 2.10"><br><br>
+
+        <h2>Dados do Cartão</h2>
+        <label>Número do cartão</label>
+        <input type="text" name="numCartao" id="numCartao"><br><br>
 
         <label>CVV do cartão</label>
-        <input type="text" name="cvvCartao" id="cvvCartao" maxlength="3"><br>
+        <input type="text" name="cvvCartao" id="cvvCartao" maxlength="3"><br><br>
 
-        <label>Mês do cartão</label>
-        <input type="text" name="mesCartao" id="mesCartao" maxlength="2"><br>
+        <label>Bandeira</label>
+        <input type="text" name="bandeiraCartao" id="bandeiraCartao"><br><br>
 
-        <label>Ano do cartão</label>
-        <input type="text" name="anoCartao" id="anoCartao" maxlength="4"><br>
+        <label>Mês de Validade</label>
+        <input type="text" name="mesCartao" id="mesCartao" maxlength="2"><br><br>
 
-        <label for="qtdParcelas">Parcelas</label>
-        <select name="qtdParcelas" id="qtdParcelas" class="qtdParcelas">
+        <label>Ano de Validade</label>
+        <input type="text" name="anoCartao" id="anoCartao" maxlength="4"><br><br>
+
+        <label>Quantidades de Parcelas</label>
+        <select name="qtdParcelas" id="qtdParcelas" class="qtd-parcelas">
             <option value="">Selecione</option>
-        </select><br>
+        </select><br><br>
 
-        <label>Valor de parcelas</label>
-        <input type="text" name="valorParcelas" id="valorParcelas"><br>
+        <label>Valor Parcelas</label>
+        <input type="text" name="valorParcelas" id="valorParcelas"><br><br>
+
+        <label>CPF do Cartão</label>
+        <input type="text" name="creditCardHolderCPF" id="creditCardHolderCPF" placeholder="CPF sem traço" required><br><br>
+
+        <label>Nome no Cartão</label>
+        <input type="text" name="creditCardHolderName" id="creditCardHolderName" placeholder="Nome igual ao escrito no cartão" required><br><br>
 
         <label>Token do cartão</label>
-        <input type="text" name="tokenCartao" id="tokenCartao" /><br>
+        <input type="text" name="tokenCartao" id="tokenCartao"><br><br>
 
-        <label>Identificador dos dados do cartão</label>
-        <input type="text" name="hashCartao" id="hashCartao" />
-        <input type="submit" name="btnComprar" id="btnComprar" value="Comprar" />
+        <label>Identificador com os dados do comprador </label>
+        <input type="text" name="hashCartao" id="hashCartao"><br><br>
+
+        <h2>Endereço do Cartão</h2>
+
+        <label>Logradouro</label>
+        <input type="text" name="billingAddressStreet" id="billingAddressStreet" placeholder="Av. Rua"><br><br>
+
+        <label>Número</label>
+        <input type="text" name="billingAddressNumber" id="billingAddressNumber" placeholder="Número"><br><br>
+
+        <label>Complemento</label>
+        <input type="text" name="billingAddressComplement" id="billingAddressComplement" placeholder="Complemento"><br><br>
+
+        <label>Bairro</label>
+        <input type="text" name="billingAddressDistrict" id="billingAddressDistrict" placeholder="Bairro"><br><br>
+
+        <label>CEP</label>
+        <input type="text" name="billingAddressPostalCode" id="billingAddressPostalCode" placeholder="CEP sem traço"><br><br>
+
+        <label>Cidade</label>
+        <input type="text" name="billingAddressCity" id="billingAddressCity" placeholder="Cidade"><br><br>
+
+        <label>Estado</label>
+        <input type="text" name="billingAddressState" id="billingAddressState" placeholder="Sigla do Estado"><br><br>
+
+        <label>País</label>
+        <input type="text" name="billingAddressCountry" id="billingAddressCountry" value="BRL"><br><br>
+
+        <input type="submit" name="btnComprar" id="btnComprar" value="Comprar">
 
     </form>
     <div class="bandeira-cartao"></div>
